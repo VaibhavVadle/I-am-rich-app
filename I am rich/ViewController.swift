61384d2f4e74972f9a3f37f85ffde7550ad8241f
VaibhavVadle <vaibhavvadle09@gmail.com>
2025-03-29 17:32:36 +0530

    Initial Commit

---
+++ I am rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by Vaibhav Vadle on 29/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
